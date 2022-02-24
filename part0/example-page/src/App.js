16c7507a7a1d61c337a9d2a4f6a97f9b828664ed
CristianVargas34 <civargas34@gmail.com>
2022-02-24 14:37:26 -0400

    Finished the video lesson #3

--- part0/example-page/src/App.js
+++ part0/example-page/src/App.js
@@ -1,28 +1,5 @@
 import { useState } from "react";
 
-function App() {
-  const [contador, setContador] = useState(1);
-
-  const handleClick = () => {
-    setContador(contador + 1);
-  };
-
-  const handleClickReset = () => {
-    setContador(0);
-  };
-
-  const isEven = contador % 2 === 0;
-  const msg = isEven ? "Es par" : "Es impar";
-
-  return (
-    <div>
-      <h1>La Pagina de Ejemplos</h1>
-      <h2>Contador: {contador}</h2>
-      <p> {msg} </p>
-      <button onClick={handleClick}>Aumentar</button>
-      <button onClick={handleClickReset}>Reset</button>
-    </div>
-  );
-}
+function App() {}
 
 export default App;
